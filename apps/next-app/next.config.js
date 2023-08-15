d3413f672c8758f4afbe5a95d97b0cb75205f309
chung-nh <56500416+chung-nh@users.noreply.github.com>
2023-08-15 10:49:19 +0700

    Update next.config.js

--- apps/next-app/next.config.js
+++ apps/next-app/next.config.js
@@ -2,4 +2,9 @@ const withTM = require('next-transpile-modules')(['ui'])
 
 module.exports = withTM({
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 })
